bd316d6e3bc269d1094dcf0877048c44c845def7
herdion marsel <herdionmarselnugriyo@gmail.com>
2018-02-01 22:43:08 +0700

    tambah loading screen

--- privasi/resources/views/index.blade.php
+++ privasi/resources/views/index.blade.php
@@ -10,7 +10,7 @@
       <h1 class="my-4" style="text-transform:uppercase;">swapi</h1>
       <div class="list-group">
         @foreach($json as $key => $data)
-            <a href="#" class="list-group-item {{$key}}">{{$key}}</a><br />
+            <a href="#" class="list-group-item {{$key}}" id="external_link">{{$key}}</a><br />
         @endforeach
       </div>
 
@@ -18,8 +18,8 @@
     <!-- /.col-lg-3 -->
 
     <div class="col-lg-9">
-
-      <div class="div1"></div>
+    <img src="{{asset('assets/img/tenor.gif')}}" id="imgLoad" style="display:none;z-index:100;" alt="loading" >
+        <div class="div1"></div>
 
     </div>
     <!-- /.col-lg-9 -->
@@ -36,9 +36,11 @@
         @foreach($json as $key => $data)
         $(document).ready(function(){
             $(".{{$key}}").click(function(){
+                $('#imgLoad').show();
                 $.ajax({
                         url: "{{$key}}",
                         success: function(result){
+                        $('#imgLoad').hide();
                         $(".div1").html(result);
                 }});
             });
@@ -50,35 +52,28 @@
             });
         });
         @endforeach
+
+        $(function(){
+
+        $('body').on('click', '#external-link', function(e)
+        {
+            e.preventDefault();
+
+            var link = $(this).attr('href');
+            
+            $('body').append(
+                '<div id="overlay">' +
+                '<img id="loading" src="http://bit.ly/pMtW1K">' +
+                '</div>'
+            );
+            
+            setTimeout(function(){
+            $('#overlay').remove();
+            window.open( link );
+            }, 2000); //2 seconds
+        });
+
+        });
 </script>
 @endsection
 
-@section('modal')
-<script>
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
-@endsection
